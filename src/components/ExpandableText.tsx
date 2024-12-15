bb0cdf73da71ee5508a4230207d6b34af5628ad3
Khaled El Hamzi <contact@elhamzi.me>
2024-12-15 16:13:08 +0100

    refactor: optimized the expandable-text component

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -8,16 +8,18 @@ interface Props {
 
 const ExpandableText = ({ children, maxChars = 180 }: Props) => {
 	const [isExpanded, setIsExpanded] = useState(false);
-	const displayedText = isExpanded ? children : children.substring(0, maxChars);
 
 	const handleToggle = () => {
 		setIsExpanded(!isExpanded);
 	};
 
+	if (children.length <= maxChars) {
+		return <div className="mt-2 px-5 text-gray-500">{children}</div>;
+	}
+
 	return (
 		<div className="mt-2 px-5 divide-y divide-slate-200 text-gray-500">
-			{displayedText}
-			{!isExpanded && '...'}
+			{isExpanded ? children : `${children.substring(0, maxChars)}...`}
 			<Button onToggleContent={handleToggle} bgColor="bg-transparent underline">
 				{isExpanded ? 'Less' : 'More'}
 			</Button>
